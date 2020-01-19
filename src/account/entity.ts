efa5272fe32772e532b456596368f3abd1a568a0
LavanyaJay <lavanyajay75@gmail.com>
2020-01-19 01:55:12 +0100

    Adding category entity and controller

--- src/account/entity.ts
+++ src/account/entity.ts
@@ -1,6 +1,6 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from "typeorm";
 import { BaseEntity } from "typeorm/repository/BaseEntity";
-import { IsString, Length } from "class-validator";
+import Category from "../category/entity";
 import { IsDecimal } from "class-validator";
 
 @Entity()
@@ -8,20 +8,16 @@ export default class Account extends BaseEntity {
   @PrimaryGeneratedColumn()
   id?: number;
 
-  @IsString()
-  @Length(1, 8)
-  @Column("text")
-  ac_type: string;
-
-  @IsString()
-  @Length(1, 200)
-  @Column("text")
-  ac_desc: string;
-
   @IsDecimal()
   @Column("decimal")
   ac_amount: number;
 
   @Column("timestamp")
   ac_date: Date;
+
+  @ManyToOne(
+    () => Category,
+    category => category.accounts
+  )
+  category: Category;
 }
--- src/db.ts
+++ src/db.ts
@@ -3,6 +3,8 @@ import { DefaultNamingStrategy } from "typeorm/naming-strategy/DefaultNamingStra
 import { NamingStrategyInterface } from "typeorm/naming-strategy/NamingStrategyInterface";
 import { snakeCase } from "typeorm/util/StringUtils";
 import Account from "./account/entity";
+import Type from "./type/entity";
+import Category from "./category/entity";
 
 class CustomNamingStrategy extends DefaultNamingStrategy
   implements NamingStrategyInterface {
@@ -35,7 +37,7 @@ export default () =>
     url:
       process.env.DATABASE_URL ||
       "postgres://postgres:secret@localhost:5434/postgres",
-    entities: [Account],
+    entities: [Account, Type, Category],
     synchronize: true,
     logging: true,
     namingStrategy: new CustomNamingStrategy()
