214c9fa8e7b5322f85a15c5a1e404d94ce4e82a7
LavanyaJay <lavanyajay75@gmail.com>
2020-02-23 10:48:38 +0100

    Retrieving cart data by month

--- src/account/controller.ts
+++ src/account/controller.ts
@@ -19,11 +19,22 @@ export default class AccountController {
     });
 
     const entity = await Account.create({ ...account, category });
+
     return entity.save();
   }
   @Get("/date/:txnDate/accounts")
   async allAccounts(@Param("txnDate") tDate: string) {
-    const account = await Account.find({ where: { ac_date: tDate } });
+    /* const account = await Account.find({ where: { ac_date: tDate } }); */
+    const account = await getRepository(Account)
+      .createQueryBuilder("acc")
+      .select("acc.category_id")
+      .addSelect("acc.id")
+      .addSelect("acc.ac_amount")
+      .addSelect("acc.ac_date")
+      .addSelect("acc.ac_remark")
+      .where("ac_date=:ac_date", { ac_date: tDate })
+      .getRawMany();
+
     return { account: account };
   }
   @Delete("/accounts/:id")
@@ -31,12 +42,14 @@ export default class AccountController {
     const account = await Account.delete({ id: id });
     return { account: account };
   }
-  @Get("/accounts/pie")
-  async calcAccounts() {
+  @Get("/accounts/pie/:monthNo")
+  async calcAccounts(@Param("monthNo") monthNo: number) {
+    console.log("month ", monthNo);
     const account = await getRepository(Account)
       .createQueryBuilder("acc")
       .select("acc.category_id")
       .addSelect("SUM(acc.ac_amount)", "sum")
+      .where("EXTRACT(MONTH FROM ac_date)=:monthId", { monthId: monthNo })
       .groupBy("category_id")
       .getRawMany();
 
